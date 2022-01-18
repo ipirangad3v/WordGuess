0662fa92b72ee526387b91b71012136349965569
Anthoni Ipiranga <anthoni@aiqfome.com>
2022-01-17 21:48:18 -0300

    changed setObservers function

--- app/src/main/java/com/ipsoft/wordguess/ui/main/MainFragment.kt
+++ app/src/main/java/com/ipsoft/wordguess/ui/main/MainFragment.kt
@@ -201,7 +201,6 @@ class MainFragment : Fragment(), View.OnClickListener {
                     fail(it)
                 }
             }
-            getRandomWord(WordRequest())
         }
     }
 
